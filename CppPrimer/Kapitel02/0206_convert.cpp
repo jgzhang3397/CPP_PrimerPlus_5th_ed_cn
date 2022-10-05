710b2f6ad1880a06a1b74b9bc14a5bc47fecff8a
jgzhang3397 <jgzhang1998@gmail.com>
2022-10-05 17:48:25 +0200

    example

---
+++ CppPrimer/Kapitel02/0206_convert.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file 0206_convert.cpp
+ * convert stone to pounds
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+int stonetolb(int); // function prototype
+
+int main()
+{
+    using namespace std;
+    int stone;
+    cout << "Enter the weight in stone: ";
+    cin >> stone;
+    int pounds = stonetolb(stone);
+    cout << stone << " stone = ";
+    cout << pounds << " pounds." << endl;
+    return 0;
+}
+
+int stonetolb(int sts)
+{
+    return 14 * sts;
+}
