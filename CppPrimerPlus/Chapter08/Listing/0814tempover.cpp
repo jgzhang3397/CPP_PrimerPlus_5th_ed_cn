a119afffee52c96cb255f43b42cab80bf0148c31
jgzhang3397 <jgzhang1998@gmail.com>
2022-12-31 17:17:22 +0100

    add

---
+++ CppPrimerPlus/Chapter08/Listing/0814tempover.cpp
@@ -0,0 +1,65 @@
+// tempover.cpp --- template overloading
+/*
+* If you remove Template B from the program, 
+* the compiler then uses Template A for listing the contents of pd,
+* so it lists the addresses instead of the values.
+*/
+
+#include <iostream>
+using namespace std;
+
+template <typename T> // template A
+void ShowArray(T arr[], int n);
+
+// template <typename T> // template B
+// void ShowArray(T * arr[], int n);
+
+struct debts
+{
+    char name[50];
+    double amount;
+};
+
+int main()
+{
+    int things[6] = {13, 31, 103, 301, 310, 130};
+    struct debts mr_E[3] = 
+    {
+        {"Ima Wolfe", 2400.0},
+        {"Ula Foxe", 4800.0},
+        {"Iby Scount", 1800.0}
+    };
+    double * pd[3];
+
+    // set pointers to the amount members of the structures in the arr mr_E
+    for(int i = 0; i < 3; i++)
+        pd[i] = &mr_E[i].amount;
+    
+    cout << "Listing Mr. E’s counts of things:\n";
+
+    // things is an array of int
+    ShowArray(things, 6); // uses template A
+    cout << "Listing Mr. E’s debts:\n";
+
+    // pd is an array of pointers to double
+    ShowArray(pd, 3); // uses template B (more specialized)
+    return 0;
+}
+
+template <typename T> // template A
+void ShowArray(T arr[], int n)
+{
+    cout << "Template A\n";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << ' ';
+    cout << endl;
+}
+
+template <typename T> // template B
+void ShowArray(T * arr[], int n)
+{
+    cout << "Template B\n";
+    for(int i = 0; i < n; i++)
+        cout << * arr[i] << ' ';
+    cout << endl;
+}
